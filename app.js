722493af4e2f3ca59c1d09b9c079751934bdb3f2
ckal <Christof.Kaller@gmail.com>
2022-10-17 14:55:01 +0000

    Update 3 files

--- app.js
+++ app.js
@@ -18,16 +18,17 @@ app.get("/", (req, res, next) => {
 app.get("/a", (req, res, next) => {
   let rawdata = fs.readFileSync("./logs/onlineLog.log");
   let student = JSON.parse(rawdata);
-  student.data.push("www", "sdf");
+  // student.data.push("www", "sdf");
   let data = JSON.stringify(student);
-  fs.writeFileSync("./logs/onlineLog.log", data);
+  //fs.writeFileSync("./logs/onlineLog.log", data);
 
   var datas = JSON.stringify(student);
   appLogger.info(rawdata);
   let obj = {
     data: [],
   };
-  // obj.table.push(student);
+  obj.data.push(student);
+  data = JSON.stringify(obj);
   // res.json(student);
   res.writeHead(200, { "Content-Type": "text/json" });
   res.write(data);
