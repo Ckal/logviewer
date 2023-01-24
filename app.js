9dd675244253300df404dc78240c7a45e3f58ae6
Christof Kaller <Christof.Kaller@gmail.com>
2023-01-24 12:21:40 +0100

    Update app.js
    Added API_Key and log file name basics. Step 1.

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const { createLogger, format, transports } = require("winston");
+const customFormat = format.combine(
+  format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+  format.align(),
+  format.printf(
+    (i) =>
+      `{"timestamp" : "${[i.timestamp]}", "level" : "${i.level}", "message": "${
+        i.message
+      }"}`
+  )
+);
+
+const onlineFormat = format.combine(format.printf((i) => `${i.message}`));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,22 +22,20 @@ app.use(express.static("logs"));
 
 const port = 3000;
 
-
-
-// TODO: make this part more dynamic 
+// TODO: make this part more dynamic
 const { appLogger, onlineLogger } = require("./utils/logger");
 
 var fs = require("fs");
 
 // Monitoring the node.js creates a page /status which is included as iframe
-app.use(require('express-status-monitor')());
+app.use(require("express-status-monitor")());
 
 // Compress when filter false https://medium.com/@victor.valencia.rico/gzip-compression-with-node-js-cc3ed74196f9
 // for node server side gzip compression of html, can be turned off by filter see const shouldCompress
-const compression = require('compression');
+const compression = require("compression");
 
 const shouldCompress = (req, res) => {
-  if (req.headers['x-no-compression']) {
+  if (req.headers["x-no-compression"]) {
     // Will not compress responses, if this header is present
     return false;
   }
@@ -32,26 +43,45 @@ const shouldCompress = (req, res) => {
   return compression.filter(req, res);
 };
 // Compress all HTTP responses
-app.use(compression({
-  // filter: Decide if the answer should be compressed or not,
-  // depending on the 'shouldCompress' function above
-  filter: shouldCompress,
-  // threshold: It is the byte threshold for the response 
-  // body size before considering compression, the default is 1 kB
-  threshold: 0
-}));
+app.use(
+  compression({
+    // filter: Decide if the answer should be compressed or not,
+    // depending on the 'shouldCompress' function above
+    filter: shouldCompress,
+    // threshold: It is the byte threshold for the response
+    // body size before considering compression, the default is 1 kB
+    threshold: 0,
+  })
+);
 
-// /////////////////////////////
-//        Root path          //
-// ////////////////////////////
+// ////////////////////////////////////////////////////////
+//        Root path  - opens the Dashboard Page         //
+// ///////////////////////////////////////////////////////
 app.get("/", (req, res, next) => {
-  res.json({
-    message: "See https://github.com/Ckal/logviewer for more detials ... ",
+  if (!process.env.API_KEY) {
+    console.log("API_KEY is not defined");
+  } else {
+    console.log("API_KEY is defined");
+    // use process.env.API_KEY in your code
+  }
+
+  fs.readFile("./public/logViewer.html", function (err, data) {
+    if (err) {
+      appLogger.severe(`Server severer error in logViewer : ${err}!` + err);
+      throw err;
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+    return;
   });
 });
 
-// Root path
-app.get("/l", (req, res, next) => {});
+app.get("/info", (req, res, next) => {
+  res.json({
+    version: "2.0.0",
+    message: "See https://github.com/Ckal/logviewer for more detials ... ",
+  });
+});
 
 // get Raw current OnlineLog.log
 app.get("/api/getRawLog", (req, res, next) => {
@@ -67,7 +97,7 @@ app.get("/api/getRawLog", (req, res, next) => {
     .replaceAll('}"]', "}]")
     .replaceAll('":["', '":[');
   //res.json(data);
-  //res.writeHead(200, { "Content-Type": "text/json" });
+  res.writeHead(200, { "Content-Type": "text/json" });
   res.write(data.removeCharAt(0).removeCharAt(data.lenght));
   res.end();
   return;
@@ -78,6 +108,7 @@ String.prototype.removeCharAt = function (i) {
   tmp.splice(i - 1, 1); // remove 1 element from the array (adjusting for non-zero-indexed counts)
   return tmp.join(""); // reconstruct the string
 };
+
 // API path to get current datatable log
 app.get("/api/getDataTableLog", (req, res, next) => {
   let rawdata = fs.readFileSync("./logs/onlineLog.log", "utf8");
@@ -88,7 +119,19 @@ app.get("/api/getDataTableLog", (req, res, next) => {
     data: [],
   };
   obj.data.push(rawdata);
-  res.json(data);
+  data = JSON.stringify(obj)
+    .replaceAll('\\"', '"')
+    .replaceAll("\\t", "")
+    .replaceAll("\\n", "")
+    .replaceAll("}{", "},{")
+    .replaceAll('["{', "[{")
+    .replaceAll('}"]', "}]")
+    .replaceAll('":["', '":[');
+  //res.json(data);
+  //res.writeHead(200, { "Content-Type": "text/json" });
+  res.write(data);
+  res.end();
+  return;
 });
 
 // API path to get current log
@@ -108,16 +151,49 @@ app.get("/api/deleteLog", (req, res, next) => {
 
 // API path to save to current log
 app.get("/api/saveLog", (req, res, next) => {
-  onlineLogger.info(req.query.message);
-  res.json({ message: "200" });
-  return;
+  // check if the API key is valid
+  console.log(process.env.API_KEY);
+  if (req.query.api_key !== process.env.API_KEY) {
+    res.status(401).json({ message: "Invalid API key" });
+    return;
+  }
+
+  // check if the log file name is provided
+  if (!req.query.log_file) {
+    res.status(400).json({
+      message: process.env.API_KEY + "Missing log file name parameter",
+    });
+    return;
+  }
+
+  // create a logger for the specified log file
+  const logger = createLogger({
+    transports: [
+      new transports.File({
+        filename: `logs/${req.query.log_file}.log`,
+        format: customFormat,
+        json: true,
+        stringify: (obj) => JSON.stringify(obj),
+      }),
+      new transports.Console({
+        format: customFormat,
+      }),
+    ],
+  });
+
+  // log the message to the specified log file
+  logger.info(req.query.message);
+
+  res.json({ message: "Log saved successfully" });
 });
 
 // Root path
 app.get("/dashboard.js", (req, res, next) => {
   fs.readFile("./public/dashboard.js", function (err, data) {
     if (err) {
-      appLogger.severe(`Server severer error in get dashboard.js  : ${err}!` + err);
+      appLogger.severe(
+        `Server severer error in get dashboard.js  : ${err}!` + err
+      );
       throw err;
     }
     res.writeHead(200, { "Content-Type": "text/javascript" });
@@ -127,22 +203,12 @@ app.get("/dashboard.js", (req, res, next) => {
   });
 });
 // Root path
-app.get("/logViewer", (req, res, next) => {
-  fs.readFile("./public/logViewer.html", function (err, data) {
-    if (err) {
-      appLogger.severe(`Server severer error in logViewer : ${err}!` + err);
-      throw err;
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-    return;
-  });
-});
-// Root path
 app.get("/logViewerFrame", (req, res, next) => {
   fs.readFile("./public/logs.html", function (err, data) {
     if (err) {
-      appLogger.severe(`Server severer error in logViewerFrame : ${err}!` + err);
+      appLogger.severe(
+        `Server severer error in logViewerFrame : ${err}!` + err
+      );
       throw err;
     }
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -152,5 +218,5 @@ app.get("/logViewerFrame", (req, res, next) => {
 });
 
 app.listen(port, () => {
-  appLogger.info(`Server Started in port : ${port}!`);
+  appLogger.info("Server Started in port : ${port}!");
 });
